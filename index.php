92c0d17e94508fc2f7eea010b9600c7f6534c6aa
chathuri kosala <11chathurikosala@gmail.com>
2025-06-03 08:42:52 +0530

    add hyper links

--- index.php
+++ index.php
@@ -32,7 +32,26 @@ include('includes/config.php');
             border-radius: 10px;
             text-align: center;
         }
+        .container {
+            position: relative;
+            max-width: 1800px;
+            margin: 0 auto;
+        }
+
+        .container img {vertical-align: middle;}
+
+        .container .content {
+            position: relative;
+            bottom: 100;
+            background: rgb(0, 0, 0); /* Fallback color */
+            background: rgba(0, 0, 0, 0.5); /* Black background with 0.5 opacity */
+            color: #f1f1f1;
+            width: 100%;
+            padding: 10px;
+
+        }
     </style>
+
     <title>Foundation Library Management System</title>
     <link rel="icon" href="img/logo.png" type="image/png">
 </head>
@@ -55,8 +74,55 @@ include('includes/config.php');
         </div>
 
     </div>
-
 </form>
+     <!-- hyper links -->
+      <div class="container">
+            <div class="content">
+                <h2 class="section-title">DATABASES</h2>
+                <span class="underline center"></span>
+                <p class="lead">You can access below databases</p>    
+                
+                <div class="container">             
+                    <div class="row p-2">
+                        <div class="col-sm-2 text-end">
+                            <h4>EMERALD</h4><a href="https://www.emerald.com/insight/"><img src="img/e.png" alt="emerald" style="width: 100px; height:90px;"></a>
+                            <br></div>
+                        <div class="col-sm-2 text-end">
+                            <h4>HEINONLINE</h4><a href="https://heinonline.org/HOL/login-hol"><img src="img/hein.png" alt="heinonline" style="width: 100px; height:90px;"></a>
+                            <br></div>
+                        <div class="col-sm-2 text-end">
+                            <h4>TURNITIN</h4><a href="https://www.turnitin.com/"><img src="img/turnitin.png" alt="turnitin" style="width: 100px; height:90px;"></a>
+                            <br></div>
+                        <div class="col-sm-2 text-end">
+                            <h4>LEEE</h4><a href="https://ieeexplore.ieee.org/Xplore/home.jsp?reload=true"><img src="img/ieee.png" alt="ieee" style="width: 100px; height:90px;"></a>
+                        </div>
+                        <div class="col-sm-2 text-end">
+                            <h4>TAYLOR & FRANCIS</h4><a href="https://www.taylorfrancis.com/"><img src="img/taylor_&_francis.png" alt="taylor" style="width: 100px; height:90px;"></a>
+                        </div>
+                        <div class="col-sm-2 text-end">
+                            <h4>RESEARC4LIFE</h4><a href="https://www.research4life.org/"><img src="img/reserach.png" alt="reserach" style="width: 100px; height:90px;"></a>
+                        </div>
+                    </div>
+
+                    <div class="row p-2">
+                        <div class="col-sm-2 text-end">
+                            <h4>HINARI</h4><a href="https://www.who.int/hinari/en/"><img src="img/hinari.png" alt="hinari" style="width: 100px; height:90px;"></a>
+                        </div>
+                        <div class="col-sm-2 text-end">
+                            <h4>IR & KDU</h4><a href="http://ir.kdu.ac.lk/"><img src="img/kdu.jpg" alt="kdu" style="width: 100px; height:90px;"></a>
+                        </div>
+                        <div class="col-sm-2 text-end">
+                            <h4>PAST PAPERS</h4><a href="http://192.248.104.12/"><img src="img/past.jpg" alt="past_paper" style="width: 100px; height:90px;"></a>
+                        </div>
+                        <div class="col-sm-2 text-end">
+                            <h4>PRESENTATION</h4><a href="#"><img src="img/pp.png" alt="presantation" style="width: 100px; height:90px;"></a>
+                        </div>
+                    </div>
+                </div>                    
+            </div>
+      </div>
+
+
 <script>
     // Array of background images
     var backgroundImages = [
